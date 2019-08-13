b85d3018c1af5a85e50a87d08717c9ab2707b46a
wyjsusan <yujuewa@gmail.com>
2019-08-12 22:17:11 -0700

    Connect to eureka naming server.

--- src/main/java/com/amituofo/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/amituofo/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,11 +2,13 @@ package com.amituofo.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.amituofo.microservices.currencyconversionservice")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
   public static void main(String[] args) {
