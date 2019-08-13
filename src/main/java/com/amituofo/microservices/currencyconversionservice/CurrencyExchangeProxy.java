ce6a1ea47b30866ee9e43d47a41b54e6eb767e35
wyjsusan <yujuewa@gmail.com>
2019-08-12 21:56:49 -0700

    Use RibbonClient to load balance the currency-exchange service.

--- src/main/java/com/amituofo/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ src/main/java/com/amituofo/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -1,11 +1,13 @@
 package com.amituofo.microservices.currencyconversionservice;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-@FeignClient(name = "currency-exchange-service", url = "http://localhost:8000")
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeProxy {
   @GetMapping("/currency-exchange/from/{from}/to/{to}")
   CurrencyConversionBean retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
